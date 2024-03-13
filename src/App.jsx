5f778af2464033cebf7fac9b8d1f45e4fbd50296
Prince <43872697+prince475@users.noreply.github.com>
2024-03-13 09:53:36 +0000

    Working with Codespaces and learning copilot usage using JavaScript Dev day learn challenge

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Prince Daniel",
+  title: "Software Engineer",
+  email: "danielmwai475@gmail.com",
+  gitHub: "prince475",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/prince-daniel-83a8aa140/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
