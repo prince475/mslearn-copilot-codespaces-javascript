1df16e7cdf46138b3877836a61c79668ee5c927f
Prince <43872697+prince475@users.noreply.github.com>
2024-03-13 10:56:04 +0000

    Updating about section with my own details.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "As a versatile FullStack developer, I excel in building React-Redux UIs and Ruby on Rails APIs to create smart and affordable solutions.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Software development",
+  "API development",
+  "API Testing using Postman & Thunder-Client",
+  "Figma Design",
+  "SEO",
+  "Business development",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about AI, IoT, and leveraging both to create not only sustainable but user centric solutions. With strong interpersonal skills and a positive attitude, I thrive as a team player, even in high-pressure situations. I prioritize learning and self-improvement, considering them crucial for career development and personal fulfillment.";
 
 const About = () => {
   return (
